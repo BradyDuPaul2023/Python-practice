7ff88c33078a38d5c4905f4666c07c0f74e2a334
Dupaul Braedon <816947@stma.k12.mn.us>
2021-09-13 15:09:23 -0500

    PythonPractive

---
+++ main.py
@@ -0,0 +1,14 @@
+#Print first movie
+print("Mugen train")
+
+#Print second movie
+print("Shang-Chi")
+
+#Print third movie
+print("Infinity war")
+
+#Print fourth movie
+print("Two heroes")
+
+#Print fifth movie
+print("DBS: Broly")
