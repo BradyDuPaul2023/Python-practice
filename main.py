f5192f203583d2b47a6b97855d07cca4d2e509bf
BradyDuPaul2023 <816947@mystma.org>
2021-09-16 15:14:31 -0500

    PythonPractice

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
-def topFiveMovies():
-    print("mugen train")
-    print("shang-chi")
-    print("infinity war")
-    print("two heroes")
-    print("DBS: Broly")
-topFiveMovies()
-print("   ")
-def topFourSports():
-    print("Soccer")
-    print("Skiing")
-    print("Swimming")
-    print("Water Tubing")
-topFourSports()
-print("   ")
-def topFiveFoods():
-    print("Alfredo")
-    print("Pizza")
-    print("Cheeseburger")
-    print("Burrito")
-    print("Chicken")
-topFiveFoods()
-print("   ")
-def myNameIs(a):
-    print(a + " is my name")
+def madLib(instrument, number, day, letter, minor, number2, number3, adjective, adjective2, fraction, adjective3, instrumentName, adjective4, noun, family, songName, adjective5, artist):
+    print("While I've stayed at home, I've practiced the " + 
+        instrument + " for " + number + " hours every " + day + 
+        ". My favorite key signature to play and practice in is " + 
+        letter + minor + ". It has " + number2 + " flats and " + number3 + " sharps. I like this key signature because it is " + 
+        adjective + " and " + adjective2 + ". I've also gotten awesome at counting time signatures. The time signature I love is " + 
+        fraction + ", because it is " + adjective3 + " to count. One instrument I want to learn how to play while im at home is " + 
+        instrumentName + ", because it is " + adjective4 + " and soungs like a " + noun + ". My " + family + 
+        " likes it when I play " + songName + ", and always gives me a round of applause after my performances! When I return to school, my teacher will be " + 
+        adjective5 + " of how great I am at playing my instrument. My teacher might even think I sound like " + artist)
 
-myNameIs("Brady")
+madLib("Trombone", "2", "wednesday", "g", "#", "2", "4", "simple", "easy", "4/4", "easy", "trumpet", "cool", "horn", "sister", "song", "amazed", "artist")
