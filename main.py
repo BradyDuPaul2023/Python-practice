a3bd174423744ad09a7e4d8a31d7096111e897b8
BradyDuPaul2023 <816947@mystma.org>
2021-09-24 15:12:57 -0500

    Python Practice

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-def madLib(instrument, number, day, letter, minor, number2, number3, adjective, adjective2, fraction, adjective3, instrumentName, adjective4, noun, family, songName, adjective5, artist):
-    print("While I've stayed at home, I've practiced the " + 
-        instrument + " for " + number + " hours every " + day + 
-        ". My favorite key signature to play and practice in is " + 
-        letter + minor + ". It has " + number2 + " flats and " + number3 + " sharps. I like this key signature because it is " + 
-        adjective + " and " + adjective2 + ". I've also gotten awesome at counting time signatures. The time signature I love is " + 
-        fraction + ", because it is " + adjective3 + " to count. One instrument I want to learn how to play while im at home is " + 
-        instrumentName + ", because it is " + adjective4 + " and soungs like a " + noun + ". My " + family + 
-        " likes it when I play " + songName + ", and always gives me a round of applause after my performances! When I return to school, my teacher will be " + 
-        adjective5 + " of how great I am at playing my instrument. My teacher might even think I sound like " + artist)
+def greaterThanTen(x):
+    if x > 10:
+        return "x is greater than 10"
+    elif x == 10:
+        return "x is 10"
+    else:
+        return "x is less than 10"
+print(greaterThanTen(11))
 
-madLib("Trombone", "2", "wednesday", "g", "#", "2", "4", "simple", "easy", "4/4", "easy", "trumpet", "cool", "horn", "sister", "song", "amazed", "artist")
+def potato(x, y):
+    if x + y > 10:
+        return "potato"
+    elif x + y == 10:
+        return "tomato"
+    else:
+        return "topato"
+
+print(potato(5, 4))
